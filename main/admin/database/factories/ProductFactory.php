c9ca40804e6a9c9d4bab71bb78ea9f6704aceb9d
Bishoy Romany <bishoy@queensenglish.co>
2023-02-02 13:13:11 +0200

    INIT Main Project

---
+++ main/admin/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(30),
+            'image' => $this->faker->imageUrl()
+        ];
+    }
+}
