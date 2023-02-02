3be73fbb4db7189db6757259681c4c55a6976b2b
Bishoy Romany <bishoy@queensenglish.co>
2023-02-02 13:05:53 +0200

    Finish admin app API

--- admin/app/Http/Controllers/ProductController.php
+++ admin/app/Http/Controllers/ProductController.php
@@ -10,4 +10,28 @@ class ProductController extends Controller
     public function index(){
         return Product::all();
     }
+
+
+    public function show(Product $product){
+        return $product;
+    }
+
+    public function store(Request $request){
+        $product = Product::create($request->only('title', 'image'));
+
+        return response($product, 201);
+    }
+
+
+    public function update(Product $product, Request $request){
+        $product->update($request->only('title', 'image'));
+
+        return response($product, 202);
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+
+        return response(null, 204);
+    }
 }
--- admin/routes/api.php
+++ admin/routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('products', [ProductController::class, 'index']);
+Route::apiResource('products', ProductController::class);
